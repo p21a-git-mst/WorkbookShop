a92a9eadab5f6ba6688d6ddc57d9254f3006793b
suc <suc@succeedy796n>
2021-05-27 11:12:03 +0900

    try-catch 修正

--- src/dao/OrderDescDAO.java
+++ src/dao/OrderDescDAO.java
@@ -10,116 +10,79 @@ import java.util.List;
 
 import model.OrderDesc;
 
-public class OrderDescDAO implements DBConfig{
+public class OrderDescDAO implements DBConfig {
 
-    public OrderDesc create(OrderDesc orderDesc) {
-        Connection conn = null;
+	static {
+		try {
+			Class.forName(DRIVER_NAME);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
+	public OrderDesc create(OrderDesc orderDesc) {
 
-        try {
-            conn = DriverManager.getConnection(JDBC_URL,DB_USER,DB_PASS);
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
 
-            String sql = "INSERT INTO order_desc (po_id,p_id,quantity,hist_p_name,hist_price) VALUES (?,?,?,?,?);";
+			String sql = "INSERT INTO order_desc (po_id,p_id,quantity,hist_p_name,hist_price) VALUES (?,?,?,?,?);";
 
+			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-            PreparedStatement pStmt=conn.prepareStatement(sql);
+			pStmt.setInt(1, orderDesc.getPo_id());
+			pStmt.setString(2, orderDesc.getP_id());
+			pStmt.setInt(3, orderDesc.getQuantity());
+			pStmt.setString(4, orderDesc.getHist_p_name());
+			pStmt.setInt(5, orderDesc.getHist_price());
 
-            pStmt.setInt(1,orderDesc.getPo_id());
-            pStmt.setString(2,orderDesc.getP_id());
-            pStmt.setInt(3,orderDesc.getQuantity());
-            pStmt.setString(4,orderDesc.getHist_p_name());
-            pStmt.setInt(5,orderDesc.getHist_price());
+			//実行
+			System.out.println(pStmt.executeUpdate());
+			return orderDesc;
 
-            //実行
-            System.out.println(pStmt.executeUpdate());
-            pStmt.close();                         // Close Statement
-            return orderDesc;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return orderDesc;
+		}
+	}
 
-        }catch(SQLException e){
-            e.printStackTrace();
-                    return orderDesc;
-        }finally {
-            if(conn !=null) {
-                try {
-                    conn.close();
+	public List<OrderDesc> selectByPo_id(Integer po_id) {
+		List<OrderDesc> orderDescList = new ArrayList<OrderDesc>();
 
-                }catch(SQLException e) {
-                    e.printStackTrace();
-                    return orderDesc;
-                }
-            }
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
 
+			String sql = "SELECT * FROM order_desc WHERE po_id=?";
 
-        }
+			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-      }
+			pStmt.setInt(1, po_id);
 
+			//実行
+			ResultSet rs = pStmt.executeQuery();
 
+			System.out.println(rs);
 
-    public List<OrderDesc> selectByPo_id(Integer po_id) {
-        Connection conn = null;
-        List<OrderDesc> orderDescList= new ArrayList<OrderDesc>();
+			while (rs.next()) {
 
-        try {
-            conn = DriverManager.getConnection(JDBC_URL,DB_USER,DB_PASS);
+				po_id = rs.getInt("po_id");
+				String p_id = rs.getString("p_id");
+				Integer quantity = rs.getInt("quantity");
+				String hist_p_name = rs.getString("hist_p_name");
+				Integer hist_price = rs.getInt("hist_price");
 
-            String sql = "SELECT * FROM order_desc WHERE po_id=?";
+				//System.out.println(p_id+";"+p_name+";"+price+";");
 
+				OrderDesc orderDescResult = new OrderDesc(po_id,
+															p_id,
+															quantity,
+															hist_p_name,
+															hist_price);
 
-            PreparedStatement pStmt=conn.prepareStatement(sql);
-
-            pStmt.setInt(1,po_id);
-
-            //実行
-            ResultSet rs=pStmt.executeQuery();
-
-            System.out.println(rs);
-
-                while(rs.next()) {
-
-
-                    po_id = rs.getInt("po_id");
-                    String p_id = rs.getString("p_id");
-                    Integer quantity = rs.getInt("quantity");
-                    String hist_p_name = rs.getString("hist_p_name");
-                    Integer hist_price = rs.getInt("hist_price");
-
-                    //System.out.println(p_id+";"+p_name+";"+price+";");
-
-                    OrderDesc orderDescResult = new OrderDesc(
-                            po_id,
-                            p_id,
-                            quantity,
-                            hist_p_name,
-                            hist_price
-                            );
-
-                    orderDescList.add(orderDescResult);
-
-                }
-            }catch(SQLException e){
-                e.printStackTrace();
-                return null;
-            }finally {
-                if(conn !=null) {
-                    try {
-                        conn.close();
-
-                    }catch(SQLException e) {
-                        e.printStackTrace();
-                        return null;
-                    }
-                }
-
-
-            }
-
-                return orderDescList;
-
-            }
-
-
-
-
+				orderDescList.add(orderDescResult);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return orderDescList;
+	}
 
 }
