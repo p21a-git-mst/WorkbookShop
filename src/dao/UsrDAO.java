be151c53922dcb07d9dfff3cc7109fde8d83dbc3
suc <suc@succeedy796n>
2021-05-27 10:53:02 +0900

    try-catch修正

--- src/dao/UsrDAO.java
+++ src/dao/UsrDAO.java
@@ -12,190 +12,141 @@ import java.util.Random;
 
 import model.Usr;
 
-public class UsrDAO implements DBConfig{
+public class UsrDAO implements DBConfig {
 
+	static {
+		try {
+			Class.forName(DRIVER_NAME);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
+	public List<Usr> findAUsr(Usr usr) {
+		List<Usr> usrList = new ArrayList<Usr>();
 
-    public List<Usr> findAUsr(Usr usr){
-        Connection conn = null;
-        List<Usr> usrList= new ArrayList<Usr>();
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
 
-        try {
-            Class.forName(DRIVER_NAME);
-            conn=DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+			String sql = "SELECT * FROM usr WHERE user_id=? and password=?;";
 
-            String sql="SELECT * FROM usr WHERE user_id=? and password=? ;";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
 
+			pStmt.setString(1, usr.getUser_id());
+			pStmt.setString(2, usr.getPassword());
 
+			ResultSet rs = pStmt.executeQuery();
 
-            PreparedStatement pStmt=conn.prepareStatement(sql);
+			System.out.println(rs);
 
-            pStmt.setString(1,usr.getUser_id());
-            pStmt.setString(2,usr.getPassword());
+			while (rs.next()) {
 
+				String user_id = rs.getString("user_id");
+				String password = rs.getString("password");
+				String l_name = rs.getString("l_name");
+				String f_name = rs.getString("f_name");
+				String l_name_kana = rs.getString("l_name_kana");
+				String f_name_kana = rs.getString("f_name_kana");
+				String prefecture = rs.getString("prefecture");
+				String city = rs.getString("city");
+				String o_address = rs.getString("o_address");
+				String tel = rs.getString("tel");
+				String email = rs.getString("email");
 
-            ResultSet rs=pStmt.executeQuery();
+				System.out.println(user_id + ";" + password + ";" + l_name + ";");
 
-            System.out.println(rs);
+				Usr usrResult = new Usr(user_id,
+										password,
+										l_name,
+										f_name,
+										l_name_kana,
+										f_name_kana,
+										prefecture,
+										city,
+										o_address,
+										tel,
+										email);
 
-                while(rs.next()) {
+				usrList.add(usrResult);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 
-                    String user_id = rs.getString("user_id");
-                    String password = rs.getString("password");
-                    String l_name = rs.getString("l_name");
-                    String f_name = rs.getString("f_name");
-                    String l_name_kana = rs.getString("l_name_kana");
-                    String f_name_kana = rs.getString("f_name_kana");
-                    String prefecture = rs.getString("prefecture");
-                    String city = rs.getString("city");
-                    String o_address = rs.getString("o_address");
-                    String tel = rs.getString("tel");
-                    String email = rs.getString("email");
+		return usrList;
+	}
 
-                    System.out.println(user_id+";"+password+";"+l_name+";");
+	public String getNewID() {
+		String newID = "";
+		int cnt = 0;
 
-                    Usr usrResult = new Usr(
-                            user_id,
-                            password,
-                            l_name,
-                            f_name,
-                            l_name_kana,
-                            f_name_kana,
-                            prefecture,
-                            city,
-                            o_address,
-                            tel,
-                            email);
+		LocalDateTime date = LocalDateTime.now();
+		if (date.getMonthValue() < 10) {
+			newID = "C0" + date.getMonthValue();
+		} else {
+			newID = "C" + date.getMonthValue();
+		}
 
-                    usrList.add(usrResult);
+		Random random = new Random();
 
-                }
-            }catch(SQLException e){
-                e.printStackTrace();
-                return null;
-            }catch(ClassNotFoundException e) {
-                e.printStackTrace();
-                return null;
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
 
-            }finally {
-                if(conn !=null) {
-                    try {
-                        conn.close();
+			do {
+				for (int i = 0; i < 4; i++) {
+					newID = newID + ((Integer) random.nextInt(9)).toString();
+				}
+				System.out.println(newID);
 
-                    }catch(SQLException e) {
-                        e.printStackTrace();
-                        return null;
-                    }
-                }
+				String sql = "SELECT count(*) AS cnt  FROM usr where usr_id=?;";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+				pStmt.setString(1, newID);
 
-            }
+				ResultSet rs = pStmt.executeQuery();
 
-                return usrList;
+				while (rs.next()) {
+					cnt = rs.getInt("cnt");
+				}
+			} while (cnt > 0);
 
-            }
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return newID;
+		}
+		return newID;
 
+	}
 
-    public String getNewID() {
-        Connection conn = null;
-        String newID="";
-        String tmpStr="";
-        int cnt=0;
+	public String insertUsr(Usr usr) {
+		String errorStr = "";
 
-        LocalDateTime date = LocalDateTime.now();
-        if(date.getMonthValue()<10) {
-            newID="C0"+date.getMonthValue();
-        }else {
-            newID="C"+date.getMonthValue();
-        }
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
 
-        Random random = new Random();
+			String sql = "INSERT INTO usr(user_id,password,l_name,f_name,l_name_kana,f_name_kana,prefecture,city,o_address,tel,email)"
+					+ " VALUES (?,?,?,?,?,?,?,?,?,?,?)";
 
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, usr.getUser_id());
+			pStmt.setString(2, usr.getPassword());
+			pStmt.setString(3, usr.getL_name());
+			pStmt.setString(4, usr.getF_name());
+			pStmt.setString(5, usr.getL_name_kana());
+			pStmt.setString(6, usr.getF_name_kana());
+			pStmt.setString(7, usr.getPrefecture());
+			pStmt.setString(8, usr.getCity());
+			pStmt.setString(9, usr.getO_address());
+			pStmt.setString(10, usr.getTel());
+			pStmt.setString(11, usr.getEmail());
 
+//			ResultSet rs = pStmt.executeQuery();
+			pStmt.executeUpdate();
 
-        try {
+		} catch (SQLException e) {
+			e.printStackTrace();
+			errorStr = "SQLエラーです。";
+			return errorStr;
+		}
+		return errorStr;
 
-            Class.forName(DRIVER_NAME);
-            conn=DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
-
-                do {
-                    for(int i=0;i<4;i++) {newID=newID+((Integer)random.nextInt(9)).toString();}
-
-                    System.out.println(newID);
-
-
-                    String sql="SELECT count(*) AS cnt  FROM usr where usr_id=?;";
-                    PreparedStatement pStmt=conn.prepareStatement(sql);
-                    pStmt.setString(1,newID);
-
-                    ResultSet rs=pStmt.executeQuery();
-
-                    while(rs.next()) {
-
-                         cnt = rs.getInt("cnt");
-
-                    }
-
-                }while(cnt>0);
-
-
-
-        }catch(SQLException e){
-            e.printStackTrace();
-            return newID;
-        }catch(ClassNotFoundException e) {
-            e.printStackTrace();
-            return newID;
-
-        }
-            return newID;
-
-        }
-
-
-
-    public String insertUsr(Usr usr) {
-        Connection conn = null;
-        String errorStr="";
-
-
-        try {
-
-            Class.forName(DRIVER_NAME);
-            conn=DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
-
-
-                    String sql="INSERT INTO usr(user_id,password,l_name,f_name,l_name_kana,f_name_kana,prefecture,city,o_address,tel,email)"
-                            + " VALUES (?,?,?,?,?,?,?,?,?,?,?)";
-                    PreparedStatement pStmt=conn.prepareStatement(sql);
-                    pStmt.setString(1,usr.getUser_id());
-                    pStmt.setString(2,usr.getPassword());
-                    pStmt.setString(3,usr.getL_name());
-                    pStmt.setString(4,usr.getF_name());
-                    pStmt.setString(5,usr.getL_name_kana());
-                    pStmt.setString(6,usr.getF_name_kana());
-                    pStmt.setString(7,usr.getPrefecture());
-                    pStmt.setString(8,usr.getCity());
-                    pStmt.setString(9,usr.getO_address());
-                    pStmt.setString(10,usr.getTel());
-                    pStmt.setString(11,usr.getEmail());
-
-                    ResultSet rs=pStmt.executeQuery();
-                    pStmt.executeUpdate();
-
-
-            }catch(SQLException e){
-                e.printStackTrace();
-                errorStr="SQLエラーです。";
-                return errorStr;
-            }catch(ClassNotFoundException e) {
-                e.printStackTrace();
-                errorStr="データベースエラーです。";
-                return errorStr;
-
-            }
-                return errorStr;
-
-        }
-
+	}
 
 }
