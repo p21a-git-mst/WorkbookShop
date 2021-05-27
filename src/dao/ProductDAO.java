689381d93b242dd28e1b282bf8ca31721e2afbc5
suc <suc@succeedy796n>
2021-05-27 10:40:20 +0900

    try-catchなど変更

--- src/dao/ProductDAO.java
+++ src/dao/ProductDAO.java
@@ -10,67 +10,47 @@ import java.util.List;
 
 import model.Product;
 
-public class ProductDAO implements DBConfig{
+public class ProductDAO implements DBConfig {
 
-    public List<Product> findAllProducts(){
-        Connection conn = null;
-        List<Product> productList= new ArrayList<Product>();
+	static {
+		try {
+			Class.forName(DRIVER_NAME);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
-        try {
-            Class.forName(DRIVER_NAME);
-            conn=DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+	public List<Product> findAllProducts() {
 
-            String sql="SELECT * FROM product ORDER BY p_id;";
+		List<Product> productList = new ArrayList<Product>();
 
-            PreparedStatement pStmt=conn.prepareStatement(sql);
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
 
-            ResultSet rs=pStmt.executeQuery();
+			String sql = "SELECT * FROM product ORDER BY p_id;";
 
-            System.out.println(rs);
+			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-                while(rs.next()) {
+			ResultSet rs = pStmt.executeQuery();
 
-                    String p_id = rs.getString("p_id");
-                    String p_name = rs.getString("p_name");
-                    Integer price = rs.getInt("price");
+			System.out.println(rs);
 
-                    //System.out.println(p_id+";"+p_name+";"+price+";");
+			while (rs.next()) {
 
-                    Product productResult = new Product(
-                            p_id,
-                            p_name,
-                            price
-                            );
+				String p_id = rs.getString("p_id");
+				String p_name = rs.getString("p_name");
+				Integer price = rs.getInt("price");
 
-                    productList.add(productResult);
+				//System.out.println(p_id+";"+p_name+";"+price+";");
 
-                }
-            }catch(SQLException e){
-                e.printStackTrace();
-                return null;
-            }catch(ClassNotFoundException e) {
-                e.printStackTrace();
-                return null;
+				Product productResult = new Product(p_id ,p_name, price);
 
-            }finally {
-                if(conn !=null) {
-                    try {
-                        conn.close();
-
-                    }catch(SQLException e) {
-                        e.printStackTrace();
-                        return null;
-                    }
-                }
-
-
-            }
-
-                return productList;
-
-            }
-
-
-
+				productList.add(productResult);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return productList;
+	}
 
 }
